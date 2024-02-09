00b9bf647fd940f7457646530011a8a8c787fac1
Roshan <roshan.k@epiance.com>
2024-02-09 16:49:27 +0000

    made ne changes

---
+++ hello.java
@@ -0,0 +1,7 @@
+echo "# intellipaat2024" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Roshan-kumar20/intellipaat2024.git
+git push -u origin main
